2a68b066bcad474a9db522e9ecbea75c039cd33d
thomas434 <tomas.temo77@gmail.com>
2021-05-05 19:12:16 +0200

    variables in go

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// var card string = "Hello World Variables in GO"
+	card := 123 // "Hello World Variables in GO"
+	fmt.Println(card)
+	card = 456 // "Hello World Variables in GO after modified"
+	fmt.Println(card)
+}
