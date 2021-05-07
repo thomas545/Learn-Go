d72604666592c2456a6883fb53df9bed060c2f20
thomas434 <tomas.temo77@gmail.com>
2021-05-07 14:26:23 +0200

    try loop and if statements

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 // func main() {
 // 	// var card string = "Hello World Variables in GO"
@@ -10,12 +12,41 @@ import "fmt"
 // 	fmt.Println(card)
 // }
 
+func add(x, y int) int {
+	return x + y
+}
+
 func main() {
 
-	cards := deck{cardName(), cardNumber()}
-	cards = append(cards, "CSV: 123")
-	cards.print()
-	fmt.Println(cards)
+	// cards := deck{cardName(), cardNumber()}
+	// cards = append(cards, "CSV: 123")
+	// cards.print()
+	// fmt.Println(cards)
+	forFace1()
+	forFace2()
+
+}
+
+func forFace1() {
+	sum := 0
+	for i := 0; i <= 10; i++ {
+		sum += i
+	}
+	fmt.Println(sum)
+}
+
+func forFace2() {
+	sum := 1
+	for sum < 100 {
+		sum += sum
+		if sum == 64 {
+			fmt.Println("if success", sum)
+			break
+		} else {
+			fmt.Println("if failed", sum)
+		}
+	}
+	fmt.Println(sum)
 }
 
 func cardName() string {
