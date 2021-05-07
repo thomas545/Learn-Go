8e556d409740bfedf562f02b07693805811b69c2
thomas434 <tomas.temo77@gmail.com>
2021-05-07 14:07:45 +0200

    custom types

---
+++ deck.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+type deck []string
+
+func (cards deck) print() {
+	for index, card := range cards {
+		fmt.Println(index, card)
+	}
+
+}
--- main.go
+++ main.go
@@ -1,13 +1,27 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
+
+// func main() {
+// 	// var card string = "Hello World Variables in GO"
+// 	card := 123 // "Hello World Variables in GO"
+// 	fmt.Println(card)
+// 	card = 456 // "Hello World Variables in GO after modified"
+// 	fmt.Println(card)
+// }
 
 func main() {
-	// var card string = "Hello World Variables in GO"
-	card := 123 // "Hello World Variables in GO"
-	fmt.Println(card)
-	card = 456 // "Hello World Variables in GO after modified"
-	fmt.Println(card)
+
+	cards := deck{cardName(), cardNumber()}
+	cards = append(cards, "CSV: 123")
+	cards.print()
+	fmt.Println(cards)
+}
+
+func cardName() string {
+	return "Master Card"
+}
+
+func cardNumber() string {
+	return "1000236547"
 }
